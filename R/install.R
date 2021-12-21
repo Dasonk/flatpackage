4204da1c38a315060ade17a45e76ad033e4a8c5f
Dason Kurkiewicz <dasonk@gmail.com>
2021-12-21 14:43:06 -0600

    Commented out rogue code so document and install don't run it every time

--- R/install.R
+++ R/install.R
@@ -1,13 +1,13 @@
-setwd("~/Downloads/tmp/")
-
-file_to_unzip <- "pacman.zip"
-
-out <- unzip(file_to_unzip)
-
-install_local <- function(path){
-    install.packages(path, repos = NULL, type = "source")
-}
-
-for(path in out){
-    install_local(path)
-}
+# setwd("~/Downloads/tmp/")
+# 
+# file_to_unzip <- "pacman.zip"
+# 
+# out <- unzip(file_to_unzip)
+# 
+# install_local <- function(path){
+#     install.packages(path, repos = NULL, type = "source")
+# }
+# 
+# for(path in out){
+#     install_local(path)
+# }
